ab9f0db8fc2e4bcce1c7ed9045e6df9003fb8f59
suntoryCodeTime <dldavidley@gmail.com>
2020-06-21 20:19:48 -0700

    feat: Implemented Controller Level Request Mapping

--- src/com/suntorycodetime/springdemo/mvc/HelloWorldController.java
+++ src/com/suntorycodetime/springdemo/mvc/HelloWorldController.java
@@ -5,8 +5,10 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/hello")
 public class HelloWorldController {
 	
 	/**Controller method to show the initial HTML form**/
@@ -32,6 +34,22 @@ public class HelloWorldController {
 		
 		//Create the message
 		String result = "Yo! " + name;
+		
+		//Add message to model
+		model.addAttribute("message", result);
+		
+		return "helloworld";
+	}
+	
+	// Controller method to read form data and add data to the model
+	@RequestMapping("/processFormVersionThree")
+	public String processFormVersionThree(@RequestParam("studentName") String name , Model model) {
+		
+		//Uppercase the data
+		name = name.toUpperCase();
+		
+		//Create the message
+		String result = "Hey My Friend from V3! " + name;
 		
 		//Add message to model
 		model.addAttribute("message", result);
